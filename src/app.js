cd74143ab3354100bec9f1a5a92d42c61d48d03b
jorge-rubinich <jr@bitbyte.com.ar>
2023-09-22 12:19:09 -0300

    Replace Ok with success

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.get("/products", async (req, res)=>{
     try {
         const results = await manager.getProducts(req.query)
         const info={"count": results.length}
-        res.status(200).send({status: "ok",info , results})
+        res.status(200).send({status: "success",info , results})
     } catch (error) {
         res.status(500).send({status: "error", error: error.message})   
     }
@@ -20,7 +20,7 @@ app.get("/products/:pid", async (req, res)=>{
     try {
         const results = await manager.getProductById(pid)
         if (results) {
-            res.status(200).send({status: "ok", results})   
+            res.status(200).send({status: "success", results})   
         }  else {
             res.status(404).send({status: "error", error: "Id "+pid+" not found"})
         }   

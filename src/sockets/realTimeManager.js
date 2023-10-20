e3b9bb9f8c0fa2a355440846a0df7b97fd77b6b0
jorge-rubinich <jr@bitbyte.com.ar>
2023-10-20 00:33:46 -0300

    Delete chat code

--- src/sockets/realTimeManager.js
+++ src/sockets/realTimeManager.js
@@ -1,15 +1,6 @@
 import manager from "../managers/productManager.js"
 
 function realTimeManager(io, socket) {
-
-    async function newUser(data) {
-      // received by new user
-      messageLogs= await chatModel.find().sort({'createdAt': -1}).limit(chatLimit)
-      messageLogs.reverse()
-      socket.emit("chat-started", messageLogs);
-      // receiver by all except new user
-      socket.broadcast.emit("user-logged-in", data);
-    }
   
     async function getProducts(data) {
         const products = await manager.getProducts({})
